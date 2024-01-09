e558a91945ebebcf3206774e26b157f9fb5f3d6a
netcam22 <annettelesage@btinternet.com>
2024-01-09 22:07:46 +0000

    fix: html issue

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -7,7 +7,7 @@ const Header :React.FC<HeaderProps>= ({image}) =>
     <header className="header">
     <img className = "header__logo" 
     alt = "Fakelandia logo with pictures of vegetables, people not talking in a lift, a red football and a mildly rude looking person" 
-    role = 'logo-image' src= {image}></img>
+    src= {image}></img>
     <Nav/>
 </header>
 export default Header;
