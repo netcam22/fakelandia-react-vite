367cb30014a23f8de78611c8384a39bf074c60d2
netcam22 <annettelesage@btinternet.com>
2024-01-09 07:12:56 +0000

    refactor: remove console logs and event prevent default from change, update form response message

--- client/src/components/form-components/form-header.tsx
+++ client/src/components/form-components/form-header.tsx
@@ -13,8 +13,11 @@ const FormHeader: React.FC<FormHeaderProps> = ({message, success, justTalked}) =
 			<p className = "form__message form__message--normal">{message}</p>
 			}
 			{success && !justTalked &&
+			<>
+			<p className = "form__message form__message--response">Thank you for your confession</p>
 			<p className = "form__message--navlink form__message--response">
 			<NavLink to = "/misdemeanours">View List of Misdemeanours</NavLink></p>
+			</>
 			}
 			{success && justTalked &&
 			<p className = "form__message form__message--normal form__message--response">{message}</p>
