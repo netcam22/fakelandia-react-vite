302fb6ec1c58a9d74a77df22c16dcbd7e5000d31
netcam22 <annettelesage@btinternet.com>
2024-01-10 07:50:55 +0000

    refactor: update routeTo type

--- client/src/hooks/use_misdemeanour_route.ts
+++ client/src/hooks/use_misdemeanour_route.ts
@@ -1,9 +1,10 @@
+import { SelectOptions } from "../data/filter_misdemeanour_form_data";
 import { MISDEMEANOURS, MisdemeanourKind } from "../types/misdemeanours.types";
 import { useParams } from "react-router-dom";
 
 const useMisdemeanourRoute = ()
-	: {[x: string]: string} | undefined => {
-		const {paramKind} = useParams() as unknown as {[x: string]: string};
+	: SelectOptions | undefined => {
+		const {paramKind} = useParams() as unknown as SelectOptions;
 		const param = paramKind as unknown as MisdemeanourKind;
 		return MISDEMEANOURS.includes(param)? {paramKind} : undefined;
 };
