3cdaa50b39083f24cb790f6a41be1973f2c95e24
netcam22 <annettelesage@btinternet.com>
2024-01-11 18:41:38 +0000

    refactor: update styling of misdemeanours with colour coding

--- client/src/components/misdemeanours/misdemeanour-item.tsx
+++ client/src/components/misdemeanours/misdemeanour-item.tsx
@@ -8,19 +8,19 @@ export type MisdemeanourItemProps = {
     indexValue: string
 }
 
- const picsumUrl = "https://picsum.photos/300/200?random=";
+const PICSUM_URL = "https://picsum.photos/300/200?random=";
  
 const MisdemeanourItem : React.FC<MisdemeanourItemProps> = ({citizenId, misdemeanour, date, indexValue}) => {
-
+    const cssClass = `row row--${misdemeanour}`; 
     return (
-    <>
+    <div className = {cssClass}>
         <div className = "cell">Citizen Id: {citizenId}</div>
         <div className = "cell">Date: {date}</div>
         <div className = "cell">Misdemeanour:  {MISDEMEANOUR_VIEWS[misdemeanour].icon}  {MISDEMEANOUR_VIEWS[misdemeanour].desc}</div>
         <div className = "cell">
-            <img alt="" className = "cell__image" src={picsumUrl+indexValue} />
+            <img alt="" className = "cell__image" src={PICSUM_URL+indexValue} />
         </div>
-    </>
+    </div>
     )
 }
 export default MisdemeanourItem;
