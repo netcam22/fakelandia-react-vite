a4a763762ddeb44de4610d9c42e312ac367f44a3
netcam22 <annettelesage@btinternet.com>
2024-01-12 10:24:34 +0000

    refactor: remove separate select input filter after refactoring

--- client/src/components/form-components/select-input.test.tsx
+++ client/src/components/form-components/select-input.test.tsx
@@ -100,3 +100,82 @@ it('calls onChange function when user selects option vegetables', () => {
 	//Assert
 	expect(mockChange).toBeCalled();
 });
+
+it('displays form label Filter when rendering filter misdemeanours form', () => {
+
+	const requiredProps : SelectInputProps = {
+		title: "Filter",
+		role: "filterMisdemeanours",
+		value: "lift",
+		onChange: () => {},
+		errorMessage: "",
+		attempted: false,
+		options: MISDEMEANOUR_FORM_OPTIONS, 
+        optionValues: MISDEMEANOUR_FORM_OPTION_VALUES
+	};
+	render(<SelectInput {...requiredProps}/>);
+
+	const labelText = screen.getByText("Filter");
+	expect(labelText).toBeInTheDocument();
+});
+
+it('displays select input field when rendering filter misdemeanours form', () => {
+    //Arrange
+	const requiredProps : SelectInputProps = {
+		title: "Filter",
+		role: "filterMisdemeanours",
+		value: "vegetables",
+		onChange: () => {},
+		errorMessage: "",
+		attempted: false,
+		options: MISDEMEANOUR_FORM_OPTIONS, 
+        optionValues: MISDEMEANOUR_FORM_OPTION_VALUES
+	};
+	//Act
+	render(<SelectInput {...requiredProps}/>);
+	const selectInput: HTMLSelectElement = screen.getByLabelText("Filter");
+	//Assert
+    expect(selectInput).toBeInTheDocument();
+});
+
+it('displays value united in filter misdemeanours form input field ', () => {
+    //Arrange
+	const requiredProps : SelectInputProps = {
+		title: "Filter",
+		role: "filterMisdemeanours",
+		value: "united",
+		onChange: () => {},
+		errorMessage: "",
+		attempted: false,
+		options: MISDEMEANOUR_FORM_OPTIONS, 
+        optionValues: MISDEMEANOUR_FORM_OPTION_VALUES
+	};
+	//Act
+	render(<SelectInput {...requiredProps}/>);
+	const selectInput: HTMLSelectElement = screen.getByLabelText("Filter");
+	//Assert
+	expect(selectInput.value).toBe("united");
+});
+
+it('calls onChange function when user selects option vegetables', () => {
+    //Arrange
+	const mockChange = vi.fn();
+	const requiredProps : SelectInputProps = {
+		title: "Filter",
+		role: "filterMisdemeanours",
+		value: "vegetables",
+		onChange: mockChange,
+		errorMessage: "",
+		attempted: false,
+		options: MISDEMEANOUR_FORM_OPTIONS, 
+        optionValues: MISDEMEANOUR_FORM_OPTION_VALUES
+	};
+	//Act
+	render(<SelectInput {...requiredProps}/>);
+	const inputField: HTMLInputElement = screen.getByLabelText("Filter");
+	if (inputField) {
+		fireEvent.change(inputField, {target: {value: 'vegetables'}})
+	}
+	//Assert
+	expect(mockChange).toBeCalled();
+});
